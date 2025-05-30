467e9bd2e71f7c6753c49345493bac45f3aa9404
Behnam <behnamstd22@gmail.com>
2025-05-31 00:44:43 +0330

    add gitignoe to all projects

--- HybridBlazor.Config/obj/Debug/net9.0/HybridBlazor.Config.AssemblyInfo.cs
+++ HybridBlazor.Config/obj/Debug/net9.0/HybridBlazor.Config.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HybridBlazor.Config")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c952c6bfa129b5f001f49d163b77cab37f4c8c67")]
 [assembly: System.Reflection.AssemblyProductAttribute("HybridBlazor.Config")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HybridBlazor.Config")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
