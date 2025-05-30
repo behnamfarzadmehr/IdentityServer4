2674f484fa0e83ceb184fc205673dbf75119c263
Behnam <behnamstd22@gmail.com>
2025-05-31 00:54:49 +0330

    remove extra ignore files

--- HybridBlazor.Shared/obj/Debug/net9.0/HybridBlazor.Shared.AssemblyInfo.cs
+++ HybridBlazor.Shared/obj/Debug/net9.0/HybridBlazor.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HybridBlazor.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c952c6bfa129b5f001f49d163b77cab37f4c8c67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e6a6d034161272ac14c8f2abf71b1f2bb295d0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HybridBlazor.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HybridBlazor.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
